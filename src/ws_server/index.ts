8ccc37ea0222a7ef608480bbf9ddd62f083a6fdd
kolirina <irinairin@list.ru>
2024-10-27 13:53:49 +0300

    feat: create ws server

---
+++ src/ws_server/index.ts
@@ -0,0 +1,18 @@
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({ port: 8081 });
+
+wss.on("connection", (ws) => {
+  console.log("New WebSocket connection established.");
+
+  ws.on("message", (message) => {
+    console.log("Received:", message.toString());
+    ws.send(`Server received: ${message}`);
+  });
+
+  ws.on("close", () => {
+    console.log("WebSocket connection closed.");
+  });
+});
+
+console.log("WebSocket server is running on ws://localhost:8081");
